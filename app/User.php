8b678aa054198b06dc11518b5881645aa0942d65
juanma051 <jmmora042@gmail.com>
2020-05-25 17:13:44 -0500

    Update User.php

--- app/User.php
+++ app/User.php
@@ -2,25 +2,55 @@
 
 namespace App;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Authenticatable
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
+    use Authenticatable, CanResetPassword;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = ['nombre', 'email', 'password', 'dependencia_id'];
 
+
+      
     /**
-     * The attributes that should be hidden for arrays.
+     * The attributes excluded from the model's JSON form.
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
+
+    public function dependencia() {
+
+        return $this->belongsTo('App\Dependencias');
+  
+    }
+
+    public function equipo() {
+
+         return $this->hasMany('App\Equipo');
+    }
+
+    public function scopeNombre($query, $nombre)
+    {
+         if(trim($nombre) != '')
+         {
+         $query->where('nombre', "LIKE", "%$nombre%");
+         }
+    }
 }
